70a8476e124ef8dc0bd6f20c931437a4d8a977da
SEIAROTg <seiarotg@gmail.com>
2018-01-27 18:16:18 +0000

    feat(order): add a trivial implementation of order

---
+++ src/order/order.h
@@ -0,0 +1,6 @@
+#ifndef PIEX_HEADER_ORDER
+#define PIEX_HEADER_ORDER
+
+#include "src/order/trivial.h"
+
+#endif
---
+++ src/order/trivial.h
@@ -0,0 +1,69 @@
+#include <cstdint>
+
+namespace piex {
+class Order {
+public:
+	using IdType = std::uint64_t;
+	using PriceType = std::uint64_t;
+	using QuantityType = std::uint64_t;
+
+	Order(const IdType &id, const PriceType &price, const QuantityType &quantity) :
+		id_(id),
+		price_(price),
+		quantity_(quantity) {
+	}
+	Order(const Order &) = default;
+	bool operator== (const Order &other) const {
+		return id_ == other.id_
+			&& price_ == other.price_
+			&& quantity_ == other.quantity_;
+	}
+
+	const IdType &id() const {
+		return id_;
+	}
+	const PriceType &price() const {
+		return price_;
+	}
+	QuantityType &quantity() {
+		return quantity_;
+	}
+	const QuantityType &quantity() const {
+		return quantity_;
+	}
+
+private:
+	IdType id_;
+	PriceType price_;
+	QuantityType quantity_;
+};
+
+class BuyOrder;
+class SellOrder;
+
+class BuyOrder : public Order {
+public:
+	using Order::Order;
+	bool operator<(const BuyOrder &other) const {
+		return (price() > other.price()) || (price() == other.price() && id() < other.id());
+	}
+	inline bool is_compatible_with(const SellOrder &) const;
+};
+
+class SellOrder : public Order {
+public:
+	using Order::Order;
+	bool operator<(const SellOrder &other) const {
+		return (price() < other.price()) || (price() == other.price() && id() < other.id());
+	}
+	inline bool is_compatible_with(const BuyOrder &) const;
+};
+
+inline bool BuyOrder::is_compatible_with(const SellOrder &sell) const {
+	return price() >= sell.price();
+}
+
+inline bool SellOrder::is_compatible_with(const BuyOrder &buy) const {
+	return price() <= buy.price();
+}
+}
