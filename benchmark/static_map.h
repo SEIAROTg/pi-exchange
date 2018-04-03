777588444b6be4302d7efc9ada81c8b71db1148d
SEIAROTg <seiarotg@gmail.com>
2018-04-03 19:10:29 +0100

    fix(benchmark): fix non strict version of static map

--- benchmark/static_map.h
+++ benchmark/static_map.h
@@ -6,12 +6,7 @@ namespace benchmark {
 
 template <class K, class V, std::size_t N>
 class static_map_base {
-public:
-	static_map_base() {
-		std::fill(std::begin(keys_), std::end(keys_), EMPTY);
-	}
 protected:
-	static constexpr K EMPTY = static_cast<K>(-1);
 	K keys_[N];
 	char values_buf_[N * sizeof(V)];
 	V (&values_)[N] = *reinterpret_cast<V(*)[N]>(values_buf_);
@@ -29,9 +24,12 @@ class static_map {};
 template <class K, class V, std::size_t N>
 class static_map<K, V, N, true> : public static_map_base<K, V, N> {
 public:
+	static_map() {
+		std::fill(std::begin(this->keys_), std::end(this->keys_), EMPTY);
+	}
 	void insert(const K &key, const V &value) {
 		std::size_t pos = this->hash(key);
-		if (this->keys_[pos] != this->EMPTY) {
+		if (this->keys_[pos] != EMPTY) {
 			this->archived_.emplace(this->keys_[pos], this->values_[pos]);
 		}
 		this->keys_[pos] = key;
@@ -41,7 +39,7 @@ public:
 	V erase(const K &key) {
 		std::size_t pos = this->hash(key);
 		if (this->keys_[pos] == key) {
-			this->keys_[pos] = this->EMPTY;
+			this->keys_[pos] = EMPTY;
 			return this->values_[pos];
 		}
 		if (key < cursor_ - N) {
@@ -53,6 +51,7 @@ public:
 		throw std::out_of_range("key not found");
 	}
 private:
+	static constexpr K EMPTY = static_cast<K>(-1);
 	K cursor_ = 0;
 };
 
@@ -60,32 +59,43 @@ private:
 template <class K, class V, std::size_t N>
 class static_map<K, V, N, false> : public static_map_base<K, V, N> {
 public:
+	static_map() {
+		std::fill(std::begin(this->keys_), std::end(this->keys_), EMPTY);
+	}
 	void insert(const K &key, const V &value) {
 		std::size_t pos = this->hash(key);
-		if (this->keys_[pos] != this->EMPTY) {
-			if (this->keys_[pos] > key) {
-				this->archived_.emplace(key, value);
-				return;
-			}
-			this->archived_.emplace(this->keys_[pos], this->values_[pos]);
+		if ((this->keys_[pos] & ~MSB) != EMPTY) {
+			this->archived_.emplace(key, value);
+			this->keys_[pos] |= MSB;
+			return;
 		}
 		this->keys_[pos] = key;
 		this->values_[pos] = value;
 	}
 	V erase(const K &key) {
 		std::size_t pos = this->hash(key);
-		if (this->keys_[pos] == key) {
-			this->keys_[pos] = this->EMPTY;
+		if ((this->keys_[pos] & ~MSB) == key) {
+			this->keys_[pos] &= ~key;
 			return this->values_[pos];
 		}
-		if (this->keys_[pos] != this->EMPTY && this->keys_[pos] > key) {
+		if (this->keys_[pos] & MSB) {
 			auto it = this->archived_.find(key);
+			if (it == this->archived_.end()) {
+				this->keys_[pos] &= ~MSB;
+				throw std::out_of_range("key not found");
+			}
 			V value = it->second;
 			this->archived_.erase(it);
 			return value;
 		}
 		throw std::out_of_range("key not found");
 	}
+private:
+	K get_key(std::size_t pos) {
+		return this->keys_[pos] & EMPTY;
+	}
+	static constexpr K MSB = ~(static_cast<K>(-1) >> 1); // most significant bit
+	static constexpr K EMPTY = static_cast<K>(-1) & (~MSB);
 };
 
 }
