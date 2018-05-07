5f3ca0a551507a254da7bbf983bc38c9cbebb63b
SEIAROTg <seiarotg@gmail.com>
2018-05-07 17:51:37 +0100

    feat(utility): add pool utility with foonathan/memory

---
+++ src/utility/pool.h
@@ -0,0 +1,51 @@
+#ifndef PIEX_HEADER_UTILITY_POOL
+#define PIEX_HEADER_UTILITY_POOL
+
+#include <foonathan/memory/config.hpp>
+#undef FOONATHAN_MEMORY_THREAD_SAFE_REFERENCE
+#define FOONATHAN_MEMORY_THREAD_SAFE_REFERENCE 0
+#include <foonathan/memory/container.hpp>
+#include <foonathan/memory/memory_pool.hpp>
+#include <foonathan/memory/namespace_alias.hpp>
+
+namespace piex {
+namespace utility {
+namespace pool {
+
+using RawAllocator = memory::memory_pool<>;
+template <class T>
+using StdAllocator = memory::std_allocator<T, RawAllocator>;
+
+template <class K, class V>
+class unordered_map {
+private:
+	RawAllocator allocator_;
+	memory::unordered_map<K, V, RawAllocator> container_;
+public:
+	explicit unordered_map(std::size_t size) :
+		allocator_(memory::unordered_map_node_size<std::pair<const K, V>>::value, size),
+		container_(allocator_) {}
+	decltype(container_) &container() {
+		return container_;
+	}
+};
+
+template <class K>
+class set {
+private:
+	RawAllocator allocator_;
+	memory::set<K, RawAllocator> container_;
+public:
+	explicit set(std::size_t size) :
+		allocator_(memory::set_node_size<K>::value, size),
+		container_(allocator_) {}
+	decltype(container_) &container() {
+		return container_;
+	}
+};
+
+}
+}
+}
+
+#endif
